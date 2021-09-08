340080ba5e19299f8cdeab64d31625648ae273d8
jeonseunghyeon <klzxklzx123@naver.com>
2021-09-08 12:53:51 +0900

    Edit
    완료

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
+//인덱스.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('potato')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
