83176aaaa2f0160f16ba317384a7617f31a45b92
jeonseunghyeon <klzxklzx123@naver.com>
2021-09-15 12:48:35 +0900

    코드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import Potato from "./Potato";
 
+const foodLike = [
+  {
+    name:'chikin',
+    image:'https://www.urbanbrush.net/web/wp-content/uploads/edd/2019/04/urbanbrush-20190417114405759881.png'
+  },
+
+  {
+    name:'ham',
+    image:'https://png.pngtree.com/element_pic/00/16/04/26571ebdaec6dfc.jpg'
+
+  }
+ 
+]
+
+function renderFood(foo) {
+  return <Food name={foo.name} picture={foo.image}/>
+}
+
+
 function App() {
   return (
     <div>
-      <h1>Hello React!!!</h1>
-      <Potato/>
+      {
+        foodLike.map(renderFood)
+      }
+
+    </div>
+  );
+}
+
+function Food({name,picture}) {
+  //console.log(props);
+  return(
+    <div>
+    <h3>I love {name}</h3>
+    <img src={picture}/>
     </div>
   );
 }
